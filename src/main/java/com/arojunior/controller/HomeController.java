7ecedae8905de3aaf5bb74f51bb9e340156ca997
Junior Oliveira <arojunior@gmail.com>
2018-03-13 23:30:16 -0300

    add demo application

---
+++ src/main/java/com/arojunior/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.arojunior.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public ResponseEntity<Void> index () {
+			return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/arojunior/repository/TweetRepository.java
@@ -0,0 +1,10 @@
+package com.arojunior.repository;
+
+import com.arojunior.model.Tweet;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends ReactiveMongoRepository<Tweet, String> {
+
+}
